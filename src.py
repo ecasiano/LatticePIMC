85fc332f1e71205af4e68377c8c1e528f8f583cf
Emanuel Casiano-Diaz <Emanuel.Casiano-Diaz@uvm.edu>
2019-07-28 12:25:13 -0400

    "Visualization now works w/o without having to delete the initial config from the data structure"

--- src.py
+++ src.py
@@ -237,6 +237,151 @@ def view_worldlines(data_struct,beta,figure_name):
     # for site_idx in range(L):
     #     N += data_struct[site_idx][-1][1] # taulist[list[(tau,N,jump_dir)]]
 
+    # Stores the initial particle configuration
+    x = []
+    for i in range(L):
+        x.append(data_struct[i][0][1])
+        # del data_struct[i][0]
+
+    print("Initial configuration: | x > = ", x)
+
+    # Spread tau,N,dir data to different arrays (hopefully will help with visualization???)
+    # Store kink times, particles after kinks, and kink directions for each site i
+    tau_list = []
+    N_list = []
+    dirs_list = []
+    for i in range(L):
+        tau_i = []
+        N_i = []
+        dirs_i = []
+        events = len(data_struct[i]) # Number of kinks at site i (includes initial config.)
+        for e in range(events):
+            tau_i.append(data_struct[i][e][0])
+            N_i.append(data_struct[i][e][1])
+            dirs_i.append(data_struct[i][e][2])
+        tau_list.append(tau_i)
+        N_list.append(N_i)
+        dirs_list.append(dirs_i)
+
+    # Initialize figure
+    plt.figure()
+
+    # Strategy: -Plot worldlines for each site, ignoring kinks
+    #           -Include kinks
+    #           -Plot tau=0 segments for each site
+
+    # Plot initial segments for each site
+    #for i in range(L):
+    #        # Occupation number of site i at tau = 0
+    #        n = x[i]
+    #        # Select line style depending on site occupation
+    #        if n == 0: ls,lw = ':',1.3
+    #        elif n == 1: ls,lw = '-',1.3
+    #        elif n == 2: ls,lw = '-',4
+    #        elif n == 3: ls,lw = '-',8
+#
+#            # Draw line with no kinks if no kinks happened
+#            if len(tau_list[i]) == 0:
+#                plt.vlines(i,0,beta,linestyle=ls,linewidth=lw)
+#            else:
+#                tau_initial = 0
+#                tau_final = tau_list[i][0]
+#                plt.vlines(i,tau_initial,tau_final,linestyle=ls,linewidth=lw)
+
+    # Plot middle segments for each site
+    for i in range(L):
+        L_tau = len(tau_list[i]) # Number of "kinks" on site i including initial config
+        for j in range(L_tau):
+#            if L_tau > 0:
+            tau_initial = tau_list[i][j]
+            if j + 1 < L_tau: # To avoid running out of range
+                tau_final = tau_list[i][j+1]
+            else:
+                tau_final = beta
+
+            n = N_list[i][j] # Occupation of the i_th site at the j_th "kink"
+            if n == 0: ls,lw = ':',1
+            elif n == 1: ls,lw = '-',1
+            elif n == 2: ls,lw = '-',3
+            elif n == 3: ls,lw = '-',5.5
+            src_site = dirs_list[i][j][0]    # Index of source site
+            dest_site = dirs_list[i][j][1]  # Index of destination site
+
+            # Draw flat regions
+            plt.vlines(i,tau_initial,tau_final,linestyle=ls,linewidth=lw)
+            # Draw worm ends
+            if src_site == dest_site and tau_initial != 0:
+                #plt.plot(i,tau_initial,marker='_',ms=5,lw=5)
+                plt.hlines(tau_initial,i-0.06,i+0.06,lw=1)
+
+            # Draw kinks
+            #src_site = dirs_list[i][j][0]    # Index of source site
+            #dest_site = dirs_list[i][j][1]  # Index of destination site
+
+            # Wrap around the spatial direction axis if kink connects the first and last sites
+            if (src_site == 0 and dest_site == L-1):
+                plt.hlines(tau_list[i][j],-0.5,0,linewidth=1)
+                plt.hlines(tau_list[i][j],L-1,L-1+0.5,linewidth=1)
+
+            elif (src_site == L-1 and dest_site == 0):
+                plt.hlines(tau_list[i][j],-0.5,0,linewidth=1)
+                plt.hlines(tau_list[i][j],L-1,L-1+0.5,linewidth=1)
+
+            else:
+                plt.hlines(tau_list[i][j],src_site,dest_site,linewidth=1)
+
+    plt.xticks(range(0,L))
+    plt.xlim(-0.5,L-1+0.5)
+    plt.ylim(0,1)
+    plt.tick_params(axis='y',which='both',left=False,right=False)
+    plt.tick_params(axis='x',which='both',top=False,bottom=False)
+    plt.xlabel(r"$i$")
+    plt.ylabel(r"$\tau/\beta$")
+    plt.savefig(file_name)
+    #plt.show()
+
+    return None
+
+# Test insert
+data_struct = [ [[0,1,(0,0)],[0.25,2,(1,0)],[0.5,1,(0,2)],[0.75,0,(0,1)]],
+                [[0,1,(1,1)],[0.25,0,(1,0)],[0.75,1,(0,1)]],
+                [[0,1,(2,2)],[0.5,2,(0,2)]] ]
+beta = 1
+is_worm_present = [False] # made flag a list so it can be passed "by reference"
+ira_loc = []    # If there's a worm present, these will store
+masha_loc = []  # the site_idx and tau_idx "by reference"
+#print("Worm present: ",is_worm_present)
+#print(data_struct)
+worm_insert(data_struct,beta,is_worm_present,ira_loc,masha_loc)
+#print(data_struct)
+#print("Worm present: ",is_worm_present, "Ira at: ",ira_loc, "Masha at: ", masha_loc)
+
+file_name = "worldlines_insert.pdf"
+view_worldlines(data_struct,beta,file_name)
+
+# Test timeshift
+worm_timeshift(data_struct,beta,is_worm_present,ira_loc,masha_loc)
+
+file_name = "worldlines_timeshift.pdf"
+#view_worldlines(data_struct,beta,file_name)
+
+# Test delete
+worm_delete(data_struct,beta,is_worm_present,ira_loc,masha_loc)
+#print(is_worm_present,ira_loc,masha_loc)
+
+'----------------------------------------------------------------------------------'
+
+def view_worldlines_old(data_struct,beta,figure_name):
+
+    # Set initial configuration
+    # Number of lattice sites
+    L = len(data_struct)
+
+    # Number of total particles in the lattice
+    # N = 0
+    # for site_idx in range(L):
+    #     N += data_struct[site_idx][-1][1] # taulist[list[(tau,N,jump_dir)]]
+
     x = []
     for i in range(L):
         x.append(data_struct[i][0][1])
@@ -338,33 +483,6 @@ def view_worldlines(data_struct,beta,figure_name):
     #plt.show()
 
     return None
-
-# Test insert
-data_struct = [ [[0,1,-26],[0.25,2,(1,0)],[0.5,1,(0,2)],[0.75,0,(0,1)]],
-                [[0,1,-26],[0.25,0,(1,0)],[0.75,1,(0,1)]],
-                [[0,1,-26],[0.5,2,(0,2)]] ]
-beta = 1
-is_worm_present = [False] # made flag a list so it can be passed "by reference"
-ira_loc = []    # If there's a worm present, these will store
-masha_loc = []  # the site_idx and tau_idx "by reference"
-#print("Worm present: ",is_worm_present)
-#print(data_struct)
-worm_insert(data_struct,beta,is_worm_present,ira_loc,masha_loc)
-#print(data_struct)
-#print("Worm present: ",is_worm_present, "Ira at: ",ira_loc, "Masha at: ", masha_loc)
-
-file_name = "worldlines_insert.pdf"
-#view_worldlines(data_struct,beta,file_name)
-
-# Test timeshift
-worm_timeshift(data_struct,beta,is_worm_present,ira_loc,masha_loc)
-
-file_name = "worldlines_timeshift.pdf"
-#view_worldlines(data_struct,beta,file_name)
-
-# Test delete
-worm_delete(data_struct,beta,is_worm_present,ira_loc,masha_loc)
-#print(is_worm_present,ira_loc,masha_loc)
 
 '----------------------------------------------------------------------------------'
 
