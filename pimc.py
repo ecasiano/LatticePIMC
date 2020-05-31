192f90dde77840b6ef995c6b84f870c459f7ab27
Emanuel Casiano-Diaz <Emanuel.Casiano-Diaz@uvm.edu>
2020-05-31 11:03:54 -0400

    Got rid of unnecessary abs in timeshift

--- pimc.py
+++ pimc.py
@@ -274,14 +274,9 @@ def worm_insert(data_struct,beta,head_loc,tail_loc,t,U,mu,eta,L,D,N,canonical,N_
     if tau_h == tau_t : return False
 
     # Determine the length of path to be modified
-#     l_path = abs(tau_h-tau_t)
     l_path = tau_h-tau_t
 
     # Determine the total particle change based on worm type
-#     if insert_worm:
-#         dN = +1 * l_path / beta
-#     else: # antiworm
-#         dN = -1 * l_path / beta
     dN = l_path/beta
 
     if canonical:
@@ -405,14 +400,9 @@ def worm_delete(data_struct,beta,head_loc,tail_loc,t,U,mu,eta,L,D,N,canonical,N_
 #         delete_anti_data[1] += 1
 
     # Determine the length of path to be modified
-    #l_path = abs(tau_h-tau_t)
     l_path = tau_h-tau_t
 
     # Determine the total particle change based on worm type
-#     if is_worm: # Delete worm
-#         dN = -1 * l_path / beta
-#     else: # Delete anti
-#         dN = +1 * l_path / beta
     dN = -l_path/beta
 
     if canonical:
@@ -558,19 +548,22 @@ def worm_timeshift(data_struct,beta,head_loc,tail_loc,t,U,mu,eta,L,D,N,canonical
 
     # Determine the length of path to be modified
     tau_old = data_struct[x][k][0] # original time of the worm end
-    l_path = abs(tau_new-tau_old)
+#     l_path = abs(tau_new-tau_old)
+    l_path = tau_new-tau_old
 
     # Determine the total particle change based on wormend to be shifted
     if shift_head:
-        if tau_new > tau_old: # advance head
-            dN = +1 * l_path / beta
-        else: # recede head
-            dN = -1 * l_path / beta
+#         if tau_new > tau_old: # advance head
+#             dN = +1 * l_path / beta
+#         else: # recede head
+#             dN = -1 * l_path / beta
+        dN = l_path/beta
     else: # shift tail
-        if tau_new > tau_old: # advance tail
-            dN = -1 * l_path / beta
-        else: # recede tail
-            dN = +1 * l_path / beta
+#         if tau_new > tau_old: # advance tail
+#             dN = -1 * l_path / beta
+#         else: # recede tail
+#             dN = +1 * l_path / beta
+        dN = -l_path/beta
 
     if canonical:
         # Reject the update if the total number is outside of (N-1,N+1)
